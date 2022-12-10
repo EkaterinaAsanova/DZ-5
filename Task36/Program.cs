294e9d2f929c04f67603c806327484b6a719234b
EkaterinaAsanova <u-kat@mail.ru>
2022-12-10 15:45:46 +0500

    Сумма элементов на нечетных позициях

---
+++ Task36/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, 
+// стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+// Диапазон любой, ввод зависит от пользователя. (размер, значения)
+Console.Write("Введите длину массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [n];
+    for (int i=0; i< array.Length; i++)
+{
+    Console.Write("Введите {0}-е число: ", i + 1);
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+int Poisk(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+       if (i % 2 == 1)
+       sum = sum + array[i];
+    }
+    return sum;
+}
+Console.WriteLine("Сумма элементов чисел на нечетных похзициях: " + Poisk(array));
